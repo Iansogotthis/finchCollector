a13ef65564ca9e13d52f07b5cccce2338b1c23f1
ianssogothis <mount88ian@gmail.com>
2024-04-07 23:43:02 -0700

    about

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    finch_data = [
+        {'name': 'House Finch', 'color': 'Red', 'habitat': 'Urban areas'},
+        {'name': 'Goldfinch', 'color': 'Yellow', 'habitat': 'Open fields and meadows'},
+        # Add more finch data as needed
+    ]
+    return render(request, 'templates/home.html', {'finch_data': finch_data})
 
-#The Home View
+from django.shortcuts import render
+
 # Define the home view
 def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
